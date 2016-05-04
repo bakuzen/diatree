6032278f8f5fb848de3a6c156e921c9efec3b1e5
Casey Kennington <bakuzen@gmail.com>
2016-05-04 13:40:22 +0200

    new API key

--- Code/diatree/src/main/java/app/Main.java
+++ Code/diatree/src/main/java/app/Main.java
@@ -71,30 +71,30 @@ public class Main {
 		
 //		for Google ASR
 		webSpeech = (GoogleASR) cm.lookup("googleASR");
-		RecoCommandLineParser rclp = new RecoCommandLineParser(new String[] {"-M", "-G", "AIzaSyAjfMedK3iDhXWA5_vlnX8YQGoFoxvy07g"});
-//		startGoogleASR(cm, rclp);
+		RecoCommandLineParser rclp = new RecoCommandLineParser(new String[] {"-M", "-G", "AIzaSyCDOkjKVuIKkMM_oic5gty7t91CWw8Y6ws"});
+		startGoogleASR(cm, rclp);
 		
 		ClientUtils.openNewClient();
 		
 //		Or, one can send words individually with a 500 ms pause between them
-		String[] uwords = {"route", "von", "bielefeld", "nach", "trier"};
+//		String[] uwords = {"route", "von", "bielefeld", "nach", "trier"};
 //		String[] uwords = {"essen", "typ", "franzözisch","ja","preis", "günstig", "wo", "stadtmitte", "rücksetzen", "anruf", "name", "michael",
 //				"rücksetzen","nachricht", "jana", "rücksetzen"};
 //		String[] uwords = {"nachricht",  "message", "nimm", "das", "rote", "kreuz","neben","dem","blauen","t", "ferkel", "name", "jana"};
 //		String[] uwords = {"hunger", "nein", "essen", "stadtmitte", "teuer", "nein", 
 //				           "teuer", "nein", "preis", "teuer","typ", "thai", 
 //				           "nein", "nein", "nein", "nein", "nein", "handy", "nein"};
-		List<String> words = Arrays.asList(uwords);
-		Thread.sleep(2000);
-//		
-		WordIU prev = WordIU.FIRST_WORD_IU;
-		for (String word : words) {
-			WordIU wiu = new WordIU(word, prev, null);
-			edits.add(new EditMessage<IU>(EditType.ADD, wiu));
-			Thread.sleep(400);
-			notifyListeners(new ArrayList<PushBuffer>(webSpeech.iulisteners));
-			prev = wiu;
-		}
+//		List<String> words = Arrays.asList(uwords);
+//		Thread.sleep(2000);
+////		
+//		WordIU prev = WordIU.FIRST_WORD_IU;
+//		for (String word : words) {
+//			WordIU wiu = new WordIU(word, prev, null);
+//			edits.add(new EditMessage<IU>(EditType.ADD, wiu));
+//			Thread.sleep(400);
+//			notifyListeners(new ArrayList<PushBuffer>(webSpeech.iulisteners));
+//			prev = wiu;
+//		}
 	}
 	
 	
